f92abe8b94a55a7f895fbe1df4793b93d94c24cc
Kelly Hum <kellyhum88@gmail.com>
2023-12-22 22:27:58 -0800

    convert to jupyter notebook

--- one-yr-wrapped.py
+++
@@ -1,9 +0,0 @@
-# turning spotify data into a visualization project
-
-import pandas as pd
-from pandas import DataFrame
-
-chars = pd.read_csv('characters.csv')
-frame = DataFrame(chars)
-
-print(frame)
